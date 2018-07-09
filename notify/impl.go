fcd8206181e71dcc9ad27bfd702dfc8f198294e6
Noah Christiano <noah@actioniq.com>
2018-07-09 12:18:20 -0400

    Fix

--- notify/impl.go
+++ notify/impl.go
@@ -519,7 +519,7 @@ func (n *PagerDuty) notifyV2(ctx context.Context, c *http.Client, eventType, key
 		return false, err
 	}
 
-	level.Info(logger).Log("url", n.conf.URL)
+	level.Info(n.logger).Log("url", n.conf.URL)
 
 	// resp, err := ctxhttp.Post(ctx, c, n.conf.URL, contentTypeJSON, &buf)
 	resp, err := ctxhttp.Post(ctx, c, "localhost:9384", contentTypeJSON, &buf)
